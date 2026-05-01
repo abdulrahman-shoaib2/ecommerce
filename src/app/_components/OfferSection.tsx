c0c2b009bee7d380e765993b4c649630a89d5d90
abdulrahman-shoaib2 <abdulrahman.shoaib012@gmail.com>
2026-05-01 19:34:51 +0300

    overflow

--- src/app/_components/OfferSection.tsx
+++ src/app/_components/OfferSection.tsx
@@ -9,7 +9,7 @@ export default function OfferSection() {
   return (
     <section className="py-10">
       <div className="container mx-auto">
-        <div className="grid md:grid-cols-2 gap-6">
+        <div className="grid md:grid-cols-2 gap-6 overflow-x-hidden">
           <div ref={ref} className={`transition-all duration-600 delay-100  ${visible?'translate-x-0 opacity-100':'-translate-x-15 opacity-0'}`}>
             <OfferCard badge={{ icon: "🔥", text: "Deal of the Day" }} title="Fresh Organic Fruits" description="Get up to 40% off on selected organic fruits" discount="40% OFF" code="ORGANIC40" link="/products" color={{from:'from-emerald-500',to:'to-emerald-700', btnIcon:'text-emerald-600'}}  />
           </div>
